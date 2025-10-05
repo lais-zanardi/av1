f31bcbd3de3c6cd66e43cbfda045b93abc9859f0
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-10-05 10:43:38 -0300

    feat: cria etapa model

--- src/model/Etapa.ts
+++ src/model/Etapa.ts
@@ -2,20 +2,73 @@ import { StatusEtapa } from "../enum/StatusEtapa"
 import Funcionario from "./Funcionario"
 
 export default class Etapa {
-    public nome: string
-    public prazo: string
-    public status: StatusEtapa
+    private nome: string
+    private prazo: string
+    private status: StatusEtapa
     public funcionarios: Funcionario[]
     
     constructor(nome: string, prazo: string, status: StatusEtapa, funcionarios: Funcionario[]) {
         this.nome = nome
         this.prazo = prazo
         this.funcionarios = funcionarios
-        this.status = status
+        this.status = StatusEtapa.PENDENTE //por default inicializar etapa como PENDENTE
+    }
+
+    public get getNome(): string {
+        return this.nome
+    }
+
+    public get getPrazo(): string {
+        return this.prazo
+    }
+
+    public get getStatus(): StatusEtapa {
+        return this.status
     }
     
-    public iniciar(): void {}
-    public finalizar(): void {}
-    public associarFuncionario(f: Funcionario): void {}
-    public listarFuncionarios(): Funcionario[] {}
+    public iniciar(): void {
+        if (this.status === StatusEtapa.PENDENTE) {
+            this.status = StatusEtapa.ANDAMENTO
+            console.log(`Etapa '${this.nome}' iniciada e em andamento.`)
+        } else {
+            throw new Error(`A etapa '${this.nome}' já está em andamento ou concluída.`)
+        }
+    }
+
+    public finalizar(): void {
+        if (this.status === StatusEtapa.ANDAMENTO) {
+            this.status = StatusEtapa.CONCLUIDA
+            console.log(`Etapa '${this.nome}' concluída com sucesso.`)
+        } else {
+            throw new Error(`A etapa '${this.nome}' não pode ser finalizada se não estiver em andamento.`)
+        }
+    }
+
+    public associarFuncionario(funcionario: Funcionario): void {
+        let jaAssociado = false
+        for (let i = 0; i < this.funcionarios.length; i++) {
+            const funcionarioAtual = this.funcionarios[i]
+
+            if (funcionarioAtual.id === funcionario.id) {
+                jaAssociado = true
+                break 
+            }
+        }
+
+        if (jaAssociado) {
+            throw new Error(`Funcionário ${funcionario.nome} já está associado a esta etapa.`)
+        }
+        
+        this.funcionarios.push(funcionario);
+        console.log(`Funcionário ${funcionario.nome} associado à etapa.`)
+    }
+
+    
+    public listarFuncionarios(): string {
+        if (this.funcionarios.length === 0) {
+            return `Nenhum funcionário designado para a etapa '${this.nome}'.`
+        }
+        const lista = this.funcionarios.map(f => `  - ${f.nome} (ID: ${f.id})`).join('\n');
+        return `Funcionários na etapa '${this.nome}':\n${lista}`;
+    }
 }
