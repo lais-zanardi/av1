cd209042205e33a16ba85c72591a6bd308577544
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-10-05 10:54:12 -0300

    feat: adiciona model de Peça

--- src/model/Peca.ts
+++ src/model/Peca.ts
@@ -2,10 +2,10 @@ import { StatusPeca } from "../enum/StatusPeca"
 import { TipoPeca } from "../enum/TipoPeca"
 
 export default class Peca {
-    public nome: string
-    public tipo: TipoPeca
-    public fornecedor: string
-    public status: StatusPeca
+    private nome: string
+    private tipo: TipoPeca
+    private fornecedor: string
+    private status: StatusPeca
     
     constructor(nome: string, tipo: TipoPeca, fornecedor: string, status: StatusPeca) {
         this.nome = nome
@@ -13,8 +13,34 @@ export default class Peca {
         this.fornecedor = fornecedor
         this.status = status
     }
+
+     public get getNome(): string {
+        return this.nome
+    }
+
+    public get getTipo(): TipoPeca {
+        return this.tipo
+    }
+
+    public get getFornecedor(): string {
+        return this.fornecedor
+    }
+
+    public get getStatus(): StatusPeca {
+        return this.status
+    }
     
-    public atualizarStatus(novoStatus: StatusPeca): void {}
-    public salvar(): void {}
-    public carregar(): void {}
+    public atualizarStatus(novoStatus: StatusPeca): void {
+        if (this.status === novoStatus) {
+            console.warn(`O status da peça '${this.nome}' já é ${novoStatus}. Nenhuma alteração foi feita.`)
+            return
+        }
+
+        this.status = novoStatus
+        console.log(`Status da peça '${this.nome}' atualizado para: ${this.status}.`)
+    }
+
+    public detalhes(): string {
+        return `Peça: ${this.nome} | Tipo: ${this.tipo} | Fornecedor: ${this.fornecedor} | Status: ${this.status}`
+    }
 }
