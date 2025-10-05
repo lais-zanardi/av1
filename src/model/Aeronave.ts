79480682eb941440c1cdddd15c3e9da13c78bacf
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-10-05 10:44:40 -0300

    feat: adiciona model de Aeronave

--- src/model/Aeronave.ts
+++ src/model/Aeronave.ts
@@ -1,11 +1,11 @@
 import { TipoAeronave } from "../enum/TipoAeronave"
 
 export default class Aeronave {
-    public codigo: string
-    public modelo: string
-    public tipo: TipoAeronave
-    public capacidade: number
-    public alcance: number
+    private codigo: string
+    private modelo: string
+    private tipo: TipoAeronave
+    private capacidade: number
+    private alcance: number
     
     constructor(codigo: string, modelo: string, tipo: TipoAeronave, capacidade: number, alcance:number) {
         this.codigo = codigo
@@ -14,8 +14,29 @@ export default class Aeronave {
         this.capacidade = capacidade
         this.alcance = alcance
     }
+    
 
-    public detalhes(): void {}
-    public salvar(): void {}
-    public carregar(): void {}
+    public get getCodigo(): string {
+        return this.codigo
+    }
+
+    public get getModelo(): string {
+        return this.modelo
+    }
+
+    public get getTipo(): TipoAeronave {
+        return this.tipo
+    }
+
+    public get getCapacidade(): number {
+        return this.capacidade
+    }
+
+    public get getAlcance(): number {
+        return this.alcance
+    }
+
+    public detalhes(): void {
+        console.log(`Código: ${this.codigo}\nModelo: ${this.modelo}\nTipo: ${this.tipo}\nCapacidade: ${this.capacidade} pessoas\nAlcance: ${this.alcance} km`)
+    }
 }
